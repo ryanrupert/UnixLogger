a3757d0f4c739d9d3459abecec9f61fd12189a25
Ryan Rupert <mudge25+code@gmail.com>
2020-04-20 21:02:22 -0700

    Add Main.java
    This is the main class that will test the logger.

---
+++ src/main/java/io/github/ryanrupert/UnixLogger/Main.java
@@ -0,0 +1,10 @@
+package io.github.ryanrupert.UnixLogger;
+
+class Main {
+    public static void main(String args[]){
+        Logger logger = Logger.create();
+        logger.crit("critical message");
+        logger.notice("notice message");
+        logger.debug("debug message");
+    }
+}
